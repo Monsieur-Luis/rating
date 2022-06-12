45a39f9044436d29c72c3dd28186ffdbcd8478f7
MonsieurLuis <gutierrezrivera@gmail.com>
2022-06-12 19:03:41 -0400

    commit un

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+
+import Container from './components/Container';
+import Heading from './components/Heading';
+import StarComponent from './components/StarComponent';
+import Button from './components/Button';
+
+
+
+function App() {
+
+  
+
+
+  const handleClick = (event) =>{
+    console.log(event);
+  }
+
+  const handleButtonClick = (event) =>{
+    alert (event.target.innerHTML);
+  }
+
+
+  return (
+    <div className="App">
+      <Container>
+      <StarComponent onClick={handleClick}></StarComponent>
+      <Button onClick={handleClick}></Button>
+      <Button onClick={handleButtonClick}></Button>
+      <Heading></Heading>
+      </Container>
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({handleClick}) => {
+  return (
+    <button onClick={handleClick}>Button</button>
+  )
+}
+
+export default Button
